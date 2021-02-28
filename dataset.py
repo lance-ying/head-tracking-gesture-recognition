b07f2329c4f186836c31a13a14cd15a6f93aa3ab
Thomas Cohn <cohnt@umich.edu>
2021-02-28 16:41:48 -0500

    Experiment with loading data from each folder. (Note that LFPW is flipped vertically, so we have to swap that axis.)

--- dataset.py
+++ dataset.py
@@ -4,10 +4,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import image
 
-filepath = "./data/300W_LP/AFW/AFW_1051618982_1_0"
+# filepath = "./data/300W_LP/AFW/AFW_1051618982_1_0"
+# filepath = "./data/300W_LP/HELEN/HELEN_100032540_1_0"
+# filepath = "./data/300W_LP/IBUG/IBUG_image_003_1_0"
+filepath = "./data/300W_LP/LFPW/LFPW_image_test_0001_0"
 img = matplotlib.image.imread(filepath + ".jpg")
 data = scipy.io.loadmat(filepath + ".mat")
 landmarks = data["pt2d"].T
+landmarks[:,1] = img.shape[1] - landmarks[:,1] # LFPW only
 
 fig, ax = plt.subplots()
 ax.imshow(img)
