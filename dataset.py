410bfef2296b83be0c1285ccea833a828c75063c
Thomas Cohn <cohnt@umich.edu>
2021-02-28 16:55:16 -0500

    Load the filepath of every image in the dataset.

--- dataset.py
+++ dataset.py
@@ -3,15 +3,45 @@ import scipy, scipy.io
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import image
+import os, os.path
 
+# Directory structure should be
+# data
+# |-- 300W_LP
+# |   |-- AFW
+# |   |   |-- AFW_#####.jpg
+# |   |   |-- AFW_#####.mat
+# |   |
+# |   |-- AFW_Flip
+# |   |   |-- AFW_#####.jpg
+# |   |   |-- AFW_#####.mat
+
+dataset_path = "./data/300W_LP/"
+
+directories_partial = ["AFW", "HELEN", "IBUG", "LFPW"]
+directories = []
+for d in directories_partial:
+	directories.append(d)
+	directories.append(d + "_Flip")
+
+image_names = []
+for d in directories:
+	raw_fnames = os.listdir(dataset_path + d)
+	for fname in raw_fnames:
+		if fname[-4:] == ".jpg":
+			image_names.append(dataset_path + d + "/" + fname[:-4])
+
+print("Found %d images" % len(image_names))
+
+filepath = image_names[np.random.randint(len(image_names))]
 # filepath = "./data/300W_LP/AFW/AFW_1051618982_1_0"
 # filepath = "./data/300W_LP/HELEN/HELEN_100032540_1_0"
 # filepath = "./data/300W_LP/IBUG/IBUG_image_003_1_0"
-filepath = "./data/300W_LP/LFPW/LFPW_image_test_0001_0"
+# filepath = "./data/300W_LP/LFPW/LFPW_image_test_0001_0"
 img = matplotlib.image.imread(filepath + ".jpg")
 data = scipy.io.loadmat(filepath + ".mat")
 landmarks = data["pt2d"].T
-landmarks[:,1] = img.shape[1] - landmarks[:,1] # LFPW only
+# landmarks[:,1] = img.shape[1] - landmarks[:,1] # LFPW only
 
 fig, ax = plt.subplots()
 ax.imshow(img)
