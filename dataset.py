dbcc4613c06a935b145cd34e18cff6a80937356c
Thomas Cohn <cohnt@umich.edu>
2021-02-28 17:01:37 -0500

    Filter out rotated versions of the images.

--- dataset.py
+++ dataset.py
@@ -28,11 +28,12 @@ image_names = []
 for d in directories:
 	raw_fnames = os.listdir(dataset_path + d)
 	for fname in raw_fnames:
-		if fname[-4:] == ".jpg":
+		if fname[-6:] == "_0.jpg":
 			image_names.append(dataset_path + d + "/" + fname[:-4])
 
 print("Found %d images" % len(image_names))
 
+# filepath = image_names[0]
 filepath = image_names[np.random.randint(len(image_names))]
 # filepath = "./data/300W_LP/AFW/AFW_1051618982_1_0"
 # filepath = "./data/300W_LP/HELEN/HELEN_100032540_1_0"
@@ -42,6 +43,11 @@ img = matplotlib.image.imread(filepath + ".jpg")
 data = scipy.io.loadmat(filepath + ".mat")
 landmarks = data["pt2d"].T
 # landmarks[:,1] = img.shape[1] - landmarks[:,1] # LFPW only
+
+# temp = "AFW_1051618982_1_0"
+# img = matplotlib.image.imread("./data/300W_LP/AFW/" + temp + ".jpg")
+# data = scipy.io.loadmat("./data/300W_LP/landmarks/AFW/" + temp + "_pts.mat")
+# landmarks = data["pts_2d"]
 
 fig, ax = plt.subplots()
 ax.imshow(img)
