45774a0c50b08729994472c45ad15f278832f7ed
Thomas Cohn <cohnt@umich.edu>
2021-02-28 17:11:59 -0500

    Fix data loading, from the landmarks folder.

--- dataset.py
+++ dataset.py
@@ -17,37 +17,28 @@ import os, os.path
 # |   |   |-- AFW_#####.mat
 
 dataset_path = "./data/300W_LP/"
+directories = ["AFW", "HELEN", "IBUG", "LFPW"]
 
-directories_partial = ["AFW", "HELEN", "IBUG", "LFPW"]
-directories = []
-for d in directories_partial:
-	directories.append(d)
-	directories.append(d + "_Flip")
-
-image_names = []
+image_fnames = []
+data_fnames = []
 for d in directories:
 	raw_fnames = os.listdir(dataset_path + d)
 	for fname in raw_fnames:
 		if fname[-6:] == "_0.jpg":
-			image_names.append(dataset_path + d + "/" + fname[:-4])
+			image_fname = dataset_path + d + "/" + fname[:-4] + ".jpg"
+			data_fname = dataset_path + "/landmarks/" + d + "/" + fname[:-4] + "_pts.mat"
+			image_fnames.append(image_fname)
+			data_fnames.append(data_fname)
 
-print("Found %d images" % len(image_names))
+print("Found %d images" % len(image_fnames))
 
-# filepath = image_names[0]
-filepath = image_names[np.random.randint(len(image_names))]
-# filepath = "./data/300W_LP/AFW/AFW_1051618982_1_0"
-# filepath = "./data/300W_LP/HELEN/HELEN_100032540_1_0"
-# filepath = "./data/300W_LP/IBUG/IBUG_image_003_1_0"
-# filepath = "./data/300W_LP/LFPW/LFPW_image_test_0001_0"
-img = matplotlib.image.imread(filepath + ".jpg")
-data = scipy.io.loadmat(filepath + ".mat")
-landmarks = data["pt2d"].T
-# landmarks[:,1] = img.shape[1] - landmarks[:,1] # LFPW only
-
-# temp = "AFW_1051618982_1_0"
-# img = matplotlib.image.imread("./data/300W_LP/AFW/" + temp + ".jpg")
-# data = scipy.io.loadmat("./data/300W_LP/landmarks/AFW/" + temp + "_pts.mat")
-# landmarks = data["pts_2d"]
+# idx = 0
+idx = np.random.randint(len(image_fnames))
+img = matplotlib.image.imread(image_fnames[idx])
+data = scipy.io.loadmat(data_fnames[idx])
+landmarks = data["pts_2d"]
+print("Image filename: %s" % image_fnames[idx])
+print("Data filename:  %s" % data_fnames[idx])
 
 fig, ax = plt.subplots()
 ax.imshow(img)
