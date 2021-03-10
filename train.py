071caa20d30da073e1ea450a96505849ec5a926d
lance-ying <lancelcy@umich.edu>
2021-03-10 22:19:24 +0800

    update learning rate

--- train.py
+++ train.py
@@ -190,7 +190,7 @@ def main():
 
 
     # defining the optimizer
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     # defining the loss function
     criterion = nn.MSELoss()
     # checking if GPU is available
