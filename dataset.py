b91423450dc2c24a1974034417e52f6bad796aaa
lance-ying <lancelcy@umich.edu>
2021-03-10 22:50:30 +0800

    update crop

--- dataset.py
+++ dataset.py
@@ -56,7 +56,7 @@ def load_data(image_fnames, data_fnames, use_loading_bar=True):
 	# tx=transforms.Compose([Rescale(250),RandomCrop(224),Normalize()])
 	# images = np.array(images)
 	# images=tx(images)
-	images=RandomCrop(10)(images)
+	images=RandomCrop((250,250))(images)
 	return images, landmarks_2d, landmarks_3d
 
 def augment_flip(images, landmarks_2d, landmarks_3d):
