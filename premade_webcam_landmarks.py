2d64d10e8154f7204afc0c6a5f436e7b61fe5e32
Thomas Cohn <cohnt@umich.edu>
2021-03-28 16:39:36 -0400

    Hacky fork of the other webcam script to use the premade detector.

---
+++ premade_webcam_landmarks.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+
+from network import load_from_checkpoint, torchify_image, detorchify_output
+from img import image_to_orientation
+from premade_detector import PremadeDetector
+
+mirror = True
+normalize = True
+model_fname = "checkpoints/normalized.checkpoint"
+
+cap = cv2.VideoCapture(0)
+# model = load_from_checkpoint(model_fname)
+
+print("Press q to quit.")
+
+# def center_crop(img):
+# 	return img[h//2 - 450//2:h//2 + 450//2, w//2 - 450//2:w//2 + 450//2]
+
+detector = PremadeDetector("shape_predictor_68_face_landmarks.dat")
+
+while(True):
+	# Grab a frame
+	ret, frame = cap.read()
+
+	if mirror:
+		frame = np.flip(frame, axis=1)
+
+	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# # Crop to 450x450 around the center
+	# h, w = gray.shape
+	# gray = center_crop(gray)
+
+	# if normalize:
+	# 	img = gray.astype(np.float32)/255
+	# 	m = np.mean(img)
+	# 	s = np.std(img)
+	# 	img -= m
+	# 	img /= s
+	# else:
+	# 	img = gray
+
+	# landmarks = detorchify_output(model(torchify_image(img))).reshape(-1,2)
+	landmarks, success = detector(frame)
+	# p1, p2 = image_to_orientation(landmarks)
+
+	annotated = np.array(frame)
+	if success:
+		for i in range(len(landmarks)):
+			x, y = landmarks[i]
+			h = int(y)
+			w = int(x)
+			cv2.circle(annotated, tuple(landmarks[i]), 5, (0,0,255), -1)
+	# cv2.line(annotated, p1, p2, (255,0,0), 2)
+
+	cv2.imshow('frame',cv2.resize(annotated, (800, 800)))
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
