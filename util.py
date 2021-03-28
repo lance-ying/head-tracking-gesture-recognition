391b2999dcdc8a8f1c7ae6a2260fe681e72a48d2
Thomas Cohn <cohnt@umich.edu>
2021-03-28 17:42:06 -0400

    Better way of concatenating arrays.

--- util.py
+++ util.py
@@ -6,9 +6,5 @@ def concatenate_seq_files(fname1, fname2, new_fname):
 		arr1 = pickle.load(f)
 	with open(fname2, "rb") as f:
 		arr2 = pickle.load(f)
-	for seq in arr1:
-		new_arr.append(seq)
-	for seq in arr2:
-		new_arr.append(seq)
 	with open(new_fname, "wb") as f:
-		pickle.dump(new_arr, f)
+		pickle.dump(arr1 + arr2, f)
